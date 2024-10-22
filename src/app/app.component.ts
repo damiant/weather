fab5d5c28872e5418d5bde90f2ee61cffa76adf4
Damian <damiantarnawsky@gmail.com>
2024-10-22 14:38:13 -0700

    feat: zip code entry

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,13 @@
 import { Component, inject, Input, signal } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { Router, RouterOutlet } from '@angular/router';
 import { WeatherService } from './weather.service';
 import { WeatherInfo } from '../models/weather-info';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, FormsModule],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
@@ -14,12 +15,19 @@ export class AppComponent {
   weatherService = inject(WeatherService);
   weatherInfo = signal<WeatherInfo>({ title: '', days: [] });
   zipCode: number | undefined;
+  enteredZip: string | undefined;
+  router = inject(Router);
   @Input()
   set zip(id: string | undefined) {
     this.zipCode = id ? parseInt(id) : 90210;
     this.update(this.zipCode);
   }
 
+
+  onSubmit() {
+    this.router.navigateByUrl(`/${this.enteredZip}`,{ replaceUrl: true });
+    this.enteredZip = '';
+  }
 
   async update(zipCode: number | undefined) {
     if (!zipCode) return;
